53a26ad4c471fbdacbc2d5ebf82df082f3610bd2
Iustin <iustin.targovet@stream-financial.com>
2019-10-02 14:53:33 +0100

    aliases initial commit

---
+++ aliases.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+set PATH = %PATH%
+DOSKEY ls=dir /B
+DOSKEY alias=vim %USERPROFILE%\Desktop\Tools\aliases.cmd
+DOSKEY dt=cd "%USERPROFILE%\Desktop"
+DOSKEY pg=cd "%USERPROFILE%\Desktop\Playground"
+DOSKEY exp=start .
+DOSKEY gti=git
+DOSKEY status= git status
+DOSKEY dev=cd "%USERPROFILE%\Desktop\dev"
+DOSKEY branch=git branch
+DOSKEY status=git status
+DOSKEY ca=git commit .
